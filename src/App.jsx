2383a30e78ad55f0675efffdbce34d81b2d00b00
Rafael Lima <srafalimacosta@gmail.com>
2024-08-09 19:09:03 -0300

    feet: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Header />
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+      />
+      <Post 
+        author="Gabriel Fulano" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+      />
     </div>
   )
 }
